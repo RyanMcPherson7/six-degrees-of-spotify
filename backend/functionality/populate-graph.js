638d69e89c38baacc07ec6d13037fb5c9aa79016
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-12-20 14:48:56 -0500

    removed useless code

--- backend/functionality/populate-graph.js
+++ backend/functionality/populate-graph.js
@@ -5,20 +5,14 @@ exports.populateGraph = (graph, map, connectionsFile) => {
   let data = fs.readFileSync(connectionsFile, { encoding: 'utf8' });
   data = data.split('\n');
 
-  // connectionsFile must not contain any empty spaces at the end
+  // connectionsFile must NOT contain any empty spaces at the end
   data.forEach((connection) => {
     const [from, to] = connection.split(' -> ');
     let [fromName, fromImageUrl] = from.split('|');
     let [toName, toImageUrl] = to.split('|');
 
     // removing '\r' character if present
-    if (fromImageUrl[fromImageUrl.length - 1] === '\r') {
-      fromImageUrl = fromImageUrl.substring(0, fromImageUrl.length - 1);
-    }
-
-    if (toImageUrl[toImageUrl.length - 1] === '\r') {
-      toImageUrl = toImageUrl.substring(0, toImageUrl.length - 1);
-    }
+    toImageUrl = toImageUrl.substring(0, toImageUrl.length - 1);
 
     graph.insert(fromName, toName);
     map.set(fromName, fromImageUrl);
