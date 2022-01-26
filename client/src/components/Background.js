51929c6839ed8676a5147cf1d51a4e6a6af055e2
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2022-01-25 19:12:02 -0500

    additional styling

---
+++ client/src/components/Background.js
@@ -0,0 +1,10 @@
+const Background = () => {
+  return (
+    <>
+        <div id='background-darkener'></div>
+        <div id='background-image'></div>
+    </>
+  );
+};
+
+export default Background;
