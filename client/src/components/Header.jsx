2a31e2b9f45086597e24925c1f89f83e0588e7e4
Ryan McPherson <ryan7mcpherson@gmail.com>
2022-08-03 22:17:54 -0400

    update header text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <header>
-    <h1>6 Degrees of Spotify</h1>
+    <h1>Six Degrees of Spotify</h1>
     <h4>
       Built by{' '}
       <a
@@ -10,14 +10,21 @@ const Header = () => (
       >
         Ryan McPherson
       </a>
+      , Powered by{' '}
+      <a
+        href="https://www.spotify.com/us/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Spotify
+      </a>
     </h4>
     <div id="description-container">
       <p id="description-message">
         Enter <span className="special">2 popular artists</span> and we'll
         generate a <span className="special">graph network</span> based on
-        Spotify's "<span className="special">fans also listen to</span>" feature
-        to see how <span className="special">connected</span> your 2 artists
-        are!
+        Spotify's "fans also listen to" feature to see how{' '}
+        <span className="special">connected</span> your 2 artists are!
       </p>
     </div>
   </header>
